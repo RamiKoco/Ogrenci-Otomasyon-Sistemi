17749a6913d824f099dcd6d782aa4b9adb5e53b5
Rami Koco <kocorami@gmail.com>
2025-09-15 16:18:08 +0300

    cc

--- src/OOS.OgrenciOtomasyonSistemi.Blazor/OgrenciOtomasyonSistemiBlazorModule.cs
+++ src/OOS.OgrenciOtomasyonSistemi.Blazor/OgrenciOtomasyonSistemiBlazorModule.cs
@@ -133,14 +133,14 @@ public class OgrenciOtomasyonSistemiBlazorModule : AbpModule
                 BlazorLeptonXLiteThemeBundles.Styles.Global,
                 bundle =>
                 {
-                    bundle.AddFiles(new BundleFile("/OOS.OgrenciOtomasyonSistemi.Blazor.styles.css", true));
+                    //bundle.AddFiles(new BundleFile("/OOS.OgrenciOtomasyonSistemi.Blazor.styles.css", true));
 
-                    bundle.AddFiles(new BundleFile("/css/site.css",true));
+                    bundle.AddFiles(new BundleFile("/css/site.css", true));
                     bundle.AddFiles(new BundleFile("/OOS.OgrenciOtomasyonSistemi.Blazor.styles.css", true));
                     bundle.AddFiles(new BundleFile("/blazor-global-styles.css"));
                     bundle.AddFiles(new BundleFile("/_content/DevExpress.Blazor.Themes/blazing-berry.bs5.min.css", true));
                     bundle.AddFiles(new BundleFile("/_content/DevExpress.Blazor.Themes/blazing-berry.bs5.css", true));
-                    //bundle.AddFiles(new BundleFile("/_content/DevExpress.Blazor.Reporting.Viewer/css/dx-blazor-reporting-components.css", true));
+                    bundle.AddFiles(new BundleFile("/_content/DevExpress.Blazor.Reporting.Viewer/css/dx-blazor-reporting-components.css", true));
                     bundle.AddFiles(new BundleFile("/_content/OOS.OgrenciOtomasyonSistemi.Core/css/component.css", true));
                 }
             );
